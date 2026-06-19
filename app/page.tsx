a9808beb6bb749bc01adb186567b08f1927137f1
isra <isranaz.cel@gmail.com>
2026-06-18 20:42:28 -0400

    We set up next.js and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    
+  <div>
+
+  <p className="text-4xl text-red-500 font-bold">hello world</p>
+
+  <Button variant="isra">Welcome</Button>
+
+  </div>
+   
+  );
+}
