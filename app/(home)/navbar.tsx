3fd057d9c62107bef251ceb2ed29d37e3e0bec43
isra <isranaz.cel@gmail.com>
2026-06-30 18:34:29 -0400

    Completed the navbar

--- app/(home)/navbar.tsx
+++ app/(home)/navbar.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
 import { Poppins } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
+import { usePathname } from 'next/navigation'
 
 const poppins = Poppins({
     subsets:["latin"],
@@ -19,10 +22,15 @@ interface NavbarItemProps{
 const NavbarItem = ({href, children, isActive}: NavbarItemProps) => {
 
     return(
-
-        <Button variant="outline">
+       
+        <Button className={cn("bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent px-3.5 text-lg",
+            isActive && "bg-black text-white hover:bg-black hover:text-white"
+        )} variant="outline">
+             <Link href={href}>
             {children}
+             </Link>
         </Button>
+       
 
     )
 }
@@ -37,6 +45,8 @@ const navbarItems =[
 
 const Navbar = () => {
     
+    const pathname = usePathname()
+
   return (
     <nav className="flex h-20 border-b justify-between font-medium bg-white">
       
@@ -49,9 +59,17 @@ const Navbar = () => {
     <div className="lg:flex items-center gap-4 hidden">
        {navbarItems.map((item)=>(
 
-        <NavbarItem key={item.href} {...item}>{item.children}</NavbarItem>
+        <NavbarItem isActive={pathname===item.href} href={item.href} key={item.href}> {item.children} </NavbarItem>
 
        ))}
+    </div>
+
+    <div className="hidden lg:flex">
+
+     <Button variant="secondary" className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"><Link href="/sign-in">Log In</Link></Button>
+
+     <Button className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:text-black hover:bg-pink-400 transition-colors text-lg"><Link href="/sign-up">Start Selling</Link></Button>
+
     </div>
 
     </nav>
